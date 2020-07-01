696cc3db2700edaca0f6a3bf6785521569aa1ec5
Taylor McKeel <josephmckeel1993@gmail.com>
2020-07-01 18:55:22 -0400

    class

--- models/index.js
+++ models/index.js
@@ -1,5 +1,16 @@
-const Person = require('./person');
-const Recipe = require('./recipe');
+const Person = require("./person");
+const Recipe = require("./recipe");
 //YOUR CODE HERE
+
+Person.hasMany(Recipe, { as: "recipes" });
+Recipe.belongsTo(Person);
+
+Person.findWithRecipes = function () {
+  return Person.findAll({ include: [{ model: Recipe }] });
+};
+
+Person.prototype.writeRecipe = function (recipe) {
+  return Recipe.create({ ...recipe, PersonId: this.id });
+};
 
 module.exports = { Person, Recipe };
